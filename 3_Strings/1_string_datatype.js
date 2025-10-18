e42ba2a6eac12072508abe3fbb695acd04fe9e0f
prasad291024 <prasadvaliv1992@gmail.com>
2025-10-18 23:07:31 +0530

    Data Types

---
+++ 3_Strings/1_string_datatype.js
@@ -0,0 +1,20 @@
+let space = ' ' // an empty space string
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+let country = 'Finland'
+let city = 'Helsinki'
+let language = 'JavaScript'
+let job = 'teacher'
+
+let fullName = firstName + space + lastName
+
+console.log(fullName) // concatenation, merging two strings together    
+console.log(firstName + ' ' + lastName) // concatenation, merging two strings together
+console.log(
+  firstName,    
+    lastName,
+    country,
+    city,
+    language,
+    job
+)
---
+++ 3_Strings/3_length.js
@@ -0,0 +1,8 @@
+// length: The string length method returns the number of characters in a string included empty space. Example:
+
+let js = 'JavaScript'
+console.log(js.length)        // 10
+let firstName = 'Wonderful'
+console.log(firstName.length) // 8
+let lastName = 'May Day'
+console.log(lastName.length)  // 7
