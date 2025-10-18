85c123e22f9c00d1afae57450ad0e7e1d5a51ae7
prasad291024 <prasadvaliv1992@gmail.com>
2025-10-18 15:56:00 +0530

    Remove Intro submodule, add as regular folder

---
+++ Intro/Script.js
@@ -0,0 +1,14 @@
+//Day 1 Intrduction to JavaScript
+//This line prints "Hello, World!" to the console
+console.log("Welcome to the world of JavaScript!");
+
+
+
+//This line declares a variable named 'greeting' and assigns it a string value   
+let greeting = "Hello, JavaScript!";
+
+//basic Variables
+let username = "Prasad"; // String variable
+let age = 30; // number variable 
+//printing values to console
+console.log("Hello, My name is " + username + " and I am "+ age + " years old.");
