15681978b8f3cbaf5618cd021859870b6b848005
prasad291024 <prasadvaliv1992@gmail.com>
2025-10-22 21:56:11 +0530

    String Methods

---
+++ 4_String_Methods/3_to_uppercase.js
@@ -0,0 +1,11 @@
+let string = 'JavaScript'
+
+console.log(string.toUpperCase())     // JAVASCRIPT
+
+let firstName = 'Jungle Book'
+
+console.log(firstName.toUpperCase())  // JUNGLE BOOK
+
+let country = 'simBa'
+
+console.log(country.toUpperCase())    // SIMBA
---
+++ 4_String_Methods/4_to_lowercase.js
@@ -0,0 +1,11 @@
+let string = 'JavaScript'
+
+console.log(string.toLowerCase())     // javascript
+
+let firstName = 'Jungle Book'
+
+console.log(firstName.toLowerCase())  // jungle book
+
+let country = 'simBa'
+
+console.log(country.toLowerCase())    // simba
